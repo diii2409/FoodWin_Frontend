82096327e14edee33292a452ad0fba7c90cba26d
diii2409 <duynhut366@gmail.com>
2024-09-09 00:56:12 +0700

    auth0 setup

--- src/components/MainNav.tsx
+++ src/components/MainNav.tsx
@@ -1,14 +1,17 @@
+import {useAuth0} from "@auth0/auth0-react";
 import clsx from "clsx";
 import {Button} from "./ui/button";
 
 export default function MainNav({className = ""}: {className?: string}) {
+	const {loginWithRedirect} = useAuth0();
 	return (
 		<Button
 			variant="ghost"
 			className={clsx(
 				"font-bold hover:text-orange-500 hover:bg-white",
 				className,
-			)}>
+			)}
+			onClick={async () => await loginWithRedirect()}>
 			Log In
 		</Button>
 	);
